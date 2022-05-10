cd46051f18350b1d3cffcecec3274b5a606da18d
andy <xxxandyfenxxx@xxx.com>
2022-05-10 15:05:30 -0700

    update

--- examples/radio-buttons.js
+++ examples/radio-buttons.js
@@ -81,35 +81,6 @@ paypal
 .PaymentFields({
   fundingSource: paypal.FUNDING.IDEAL,
   style: {
-    variables: {
-      fontFamily: "'Helvetica Neue', Arial, sans-serif",
-      fontSizeBase: "0.9375rem",
-      fontSizeSm: "0.93rem",
-      fontSizeM: "0.93rem",
-      fontSizeLg: "1.0625rem",
-      textColor: "#2c2e2f",
-      colorTextPlaceholder: "#2c2e2f",
-      colorBackground: "#fff",
-      colorInfo: "#0dcaf0",
-      colorDanger: "#d20000",
-      borderRadius: "0.2rem",
-      borderColor: "#dfe1e5",
-      borderWidth: "1px",
-      borderFocusColor: "black",
-      spacingUnit: "10px",
-    },
-    rules: {
-      ".Input": {},
-      ".Input:hover": {},
-      ".Input:focus": {
-      },
-      ".Input:active": {},
-      ".Input--invalid": {},
-      ".Label": {},
-      ".Error": {
-        marginTop: '2px',
-      },
-    },
   },
   fields: {
     name: {
