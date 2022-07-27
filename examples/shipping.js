57c8a2a1037ab81251887a87eef8e48fe9a4b2ef
andy <test15422@noreply.com>
2022-07-27 11:53:07 -0700

    log

--- examples/shipping.js
+++ examples/shipping.js
@@ -202,3 +202,39 @@ paypal
     },
   })
   .render("#applepay-btn");
+
+
+  function logResponse(title, response) {
+    const formattedResponse = document.createElement("pre");
+    formattedResponse.style.marginBottom = "3rem"
+  
+    const code = document.createElement("code");
+    code.classList.add("language-json");
+  
+    formattedResponse.appendChild(code);
+  
+    const formattedTitle = document.createElement("h6");
+    formattedTitle.style.marginLeft = "1rem"
+  
+    code.appendChild(
+      document.createTextNode(JSON.stringify(response, null, 2))
+    );
+  
+    formattedTitle.appendChild(document.createTextNode(title));
+  
+    document
+      .getElementById("logging")
+      .insertAdjacentElement("afterend", formattedResponse);
+  
+    document
+      .getElementById("logging")
+      .insertAdjacentElement("afterend", formattedTitle);
+  
+      // eslint-disable-next-line
+      hljs.configure({
+        ignoreUnescapedHTML: true
+      });
+  
+      // eslint-disable-next-line
+      hljs.highlightAll();
+  }
